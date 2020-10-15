e0be685240c964f957da26ae9699cbf8f3326e29
Brandon O <b.ondayko@gmail.com>
2020-10-14 22:22:29 -0500

    Fixed bug where generator started immediatly

--- script.js
+++ script.js
@@ -111,4 +111,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", writePassword);
