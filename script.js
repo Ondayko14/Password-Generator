32bc6e07f7096e8c6c7a1f99e9d3c00aaef7f3e1
Brandon O <b.ondayko@gmail.com>
2020-10-15 12:32:27 -0500

    Added comments to clarify pieces of the generatePassword function

--- script.js
+++ script.js
@@ -1,75 +1,7 @@
-// Assignment code here
 
-/*//Determines the length of the password. Options are (8-128". Return an integer of 8-128 depending on the user input.)
-var passLength = function() {
-    var lengthQuestion = parseInt(window.prompt("How long would you like your password to be? Please insert a number between 8-128"));
-    if (lengthQuestion >= 8 && lengthQuestion <= 128) {
-        var lengthConfirmation = window.confirm("Your password will be " + lengthQuestion + " characters long. is this okay?");
-        if (lengthConfirmation) {
-            console.log(lengthQuestion);
-            return lengthQuestion;
-        } else {
-            passLength();
-        }
-    } else {
-        window.alert("Invalid input, please insert a number 8-128. Please note it should be input as '8', '9', '20', ect...");
-        passLength();
-    }
-}
-//Determines wether or not to use special characters in the password. (charsetAll = "yes" charsetNo = "no") Returns "yes" or "no"
-var charSelect = function() {
-    var charQuestion = window.prompt("Would you like to use special characters in the creation  of this password? please enter YES or NO");
-    if (charQuestion.toLowerCase() === "yes") {
-        var charConfirmation = window.confirm("Are you sure you would like to use special characters?");
-        if (charConfirmation) {
-            window.alert("Special Characters have been selected");
-            console.log("yes");
-            return "yes";
-        } else {
-            charSelect();
-        }
-    } else if (charQuestion.toLowerCase() === "no") {
-        var charConfirmation = window.confirm("Are you sure that you do not want to use special characters?");
-        if (charConfirmation) {
-            window.alert("Special Characters have been turned off");
-            console.log("no");
-            return "no";
-        } else {
-            charSelect();
-        }
-    } else {
-        window.alert("Invalid input. Please answer Yes or No");
-        charSelect();
-    }
-     
-}
-//Generates a random number based off of charset and charsetAll. Use randomGenerator(1) to access charset. Use randomGenerator() to access charsetAll.
-var randomNumberGenerator = function(charsetselection) {
-    if (charsetselection === 1) {
-        var randomNumber = Math.floor(Math.random() * charset.length);
-        return randomNumber; 
-    } else {
-        var randomNumber = Math.floor(Math.random() * charsetAll.length);
-        return randomNumber;
-    }
-
-}
-*/
-/* var finalLength = passLength();
-    var finalChar = charSelect();
-    if (finalChar === "yes") {
-        for (var i = 0; i < finalLength; i++) {
-            console.log(charsetAll[randomNumberGenerator()]);
-        }
-    } else {
-        for (var i = 0; i < finalLength; i++) {
-            console.log(charset[randomNumberGenerator(1)]);
-        }
-    }
-    */
-
-
+//Password Generator Engine
 var generatePassword = function () {
+    //Prompts for the If Statements
     var howLong = window.prompt("How long would you like your password to be? please choose a number between 8-128");
     var length = parseInt(howLong);
     window.alert("You have selected your character length");
@@ -101,9 +33,11 @@ var generatePassword = function () {
     var charsetNumericUpperSpecial = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "`", "{", "|", "}", "~", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     var charsetNumericAll = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "`", "{", "|", "}", "~", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var charsetNumericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    //End Result
+    //End Result will display here
     var result = "";
 
+
+    //If Statments to logically deduce what the CharFinal will be. It can be any of the local variables placed above
     if (charSelect && lowerCapSelect && !upperCapSelect && !numericSelect) {
         charFinal = charsetLowerSpecial;
     } else if (charSelect && upperCapSelect && !lowerCapSelect && !numericSelect) {
@@ -138,6 +72,7 @@ var generatePassword = function () {
         window.alert("Invalid Input, Please Try Again");
     }
 
+    //Iterates thru the charFinal that was decided above and returns the letters as a string. Iteration length is from the "length prompt".
     if (length >= 8 && length <= 128) {
         for (i = 0; i < length; i++) {
             console.log(charFinal[Math.floor(Math.random() * charFinal.length)]);
@@ -147,6 +82,8 @@ var generatePassword = function () {
             window.alert("Incorrect input, please try again");
             
         };
+
+    //returns the "result variable found underneath the charset Variables"
     return result;
 
 
